003d7e7aa52343c5d3ee2f479e4d678e41b37ed6
Jonas Antunes <jonas.lima@grupogaia.com.br>
2020-12-30 17:16:55 -0300

    form error

--- pages/players/form.js
+++ pages/players/form.js
@@ -28,62 +28,64 @@ const Form = ({ preloadedValues, playerId }) => {
 
   return (
     <>
-      <form
-        className={`edit-player${busy ? " loading" : ""}`}
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <div className="collumn avatar">
-          <h1>{preloadedValues.name}</h1>
-          <div className="avatar-input-group">
-            <Image
-              src="/portaitDefault.jpg"
-              alt="avatar"
-              height="110px"
-              width="110px"
-            />
-            <label htmlFor="avatar">
-              <i className="fas fa-edit"></i>
-            </label>
-            <input type="hidden" name="avatar" id="avatar" />
-            <div className="ring"></div>
+      {preloadedValues ? (
+        <form
+          className={`edit-player${busy ? " loading" : ""}`}
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <div className="collumn avatar">
+            <h1>{preloadedValues.name}</h1>
+            <div className="avatar-input-group">
+              <Image
+                src="/portaitDefault.jpg"
+                alt="avatar"
+                height="110px"
+                width="110px"
+              />
+              <label htmlFor="avatar">
+                <i className="fas fa-edit"></i>
+              </label>
+              <input type="hidden" name="avatar" id="avatar" />
+              <div className="ring"></div>
+            </div>
           </div>
-        </div>
-        <div className="collumn inputs">
-          <div className="input-group">
-            <label htmlFor="name">nome</label>
+          <div className="collumn inputs">
+            <div className="input-group">
+              <label htmlFor="name">nome</label>
+              <input
+                className={errors.name ? "error" : ""}
+                name="name"
+                id="name"
+                ref={register({ required: true })}
+              />
+            </div>
+            <div className="input-group">
+              <label htmlFor="natFiveOwned"># NAT5!</label>
+              <input
+                className={errors.natFiveOwned ? "error" : ""}
+                name="natFiveOwned"
+                id="natFiveOwned"
+                ref={register({ required: true })}
+              />
+            </div>
+            <div className="input-group">
+              <label htmlFor="creationDate">data de criação</label>
+              <input
+                className={errors.creationDate ? "error" : ""}
+                name="creationDate"
+                id="creationDate"
+                ref={register({ required: true })}
+              />
+            </div>
             <input
-              className={errors.name ? "error" : ""}
-              name="name"
-              id="name"
-              ref={register({ required: true })}
+              type="submit"
+              disabled={busy}
+              value={busy ? "Carregando..." : "Atualizar"}
             />
           </div>
-          <div className="input-group">
-            <label htmlFor="natFiveOwned"># NAT5!</label>
-            <input
-              className={errors.natFiveOwned ? "error" : ""}
-              name="natFiveOwned"
-              id="natFiveOwned"
-              ref={register({ required: true })}
-            />
-          </div>
-          <div className="input-group">
-            <label htmlFor="creationDate">data de criação</label>
-            <input
-              className={errors.creationDate ? "error" : ""}
-              name="creationDate"
-              id="creationDate"
-              ref={register({ required: true })}
-            />
-          </div>
-          <input
-            type="submit"
-            disabled={busy}
-            value={busy ? "Carregando..." : "Atualizar"}
-          />
-        </div>
-        <Loading></Loading>
-      </form>
+          <Loading></Loading>
+        </form>
+      ) : null}
       <div className={`updatedMessage${updated ? " show" : ""}`}>
         Atualizado com sucesso!
       </div>
